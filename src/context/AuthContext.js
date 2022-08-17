7a8159cf70fd63e6ba1023d69ff5026a38faf326
Jonatas Braz de Sousa <jonatasbraz@gmail.com>
2022-08-17 10:56:55 -0300

    salvar tokens nos cookies

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
-import Router from 'next/router';
 import { createContext, useState } from 'react';
+import { setCookie } from 'nookies';
+import Router from 'next/router';
 import { api } from '../../services/api';
 
 export const AuthContext = createContext({});
@@ -13,7 +14,16 @@ export function AuthProvider({ children })  {
       const response = await api.post('auth', {
         email, password
       });
-      const { permissions, roles } = response.data;
+      const { access_token, refresh_token, permissions, roles } = response.data;
+
+      setCookie(undefined, '@web-app-access-token', access_token, {
+        maxAge: 60 * 60 * 24,
+        path: '/'
+      });
+      setCookie(undefined, '@web-app-refresh-token', refresh_token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/'
+      });
       setUser({ email, permissions, roles });
 
       Router.push('/dashboard')
