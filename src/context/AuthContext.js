841468add1a3628ddb22b666b728e7e9757781b1
Jonatas Braz de Sousa <jonatasbraz@gmail.com>
2022-08-17 11:18:05 -0300

    recuperar estado da autenticação mesmo após atualizar página

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
-import { createContext, useState } from 'react';
-import { setCookie } from 'nookies';
+import { createContext, useEffect, useState } from 'react';
+import { parseCookies, setCookie } from 'nookies';
 import Router from 'next/router';
 import { api } from '../../services/api';
 
@@ -8,6 +8,16 @@ export const AuthContext = createContext({});
 export function AuthProvider({ children })  {
   const [user, setUser] = useState();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { '@web-app-access-token': access_token } = parseCookies();
+    if(access_token) {
+      api.get('/profile').then(response => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
 
   async function signIn({ email, password }) {
     try {
@@ -26,6 +36,8 @@ export function AuthProvider({ children })  {
       });
       setUser({ email, permissions, roles });
 
+      api.defaults.headers['Authorization'] = `Bearer ${access_token}`;
+      
       Router.push('/dashboard')
     } catch(err) {
       console.log(err);
