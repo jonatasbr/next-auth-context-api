eae05eeb4e9e373ff75215a6c619e65fb21a883b
Jonatas Braz de Sousa <jonatasbraz@gmail.com>
2022-08-17 17:08:11 -0300

    criacao de hook de permissao

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,9 +3,14 @@ import { AuthContext } from '../context/AuthContext';
 import { withSSRAuth } from '../utils/withSSRAuth';
 import { getApi } from '../services/api';
 import { api } from '../services/apiClient';
+import { useCan } from '../hooks/useCan';
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user  } = useContext(AuthContext);
+
+  const userIsAdmin = useCan({
+    roles: ['administrator']
+  });
 
   useEffect(() => {
     api.get('/profile')
@@ -14,7 +19,9 @@ export default function Dashboard() {
   }, []);
   return (
     <div>
-      <h1>Dashboard | {user?.email} </h1>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userIsAdmin && <div>Usuário admin</div> }
     </div>
   );
 }
